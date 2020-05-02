c79ed1412b6d4ed408a7e538555ecfd1af596812
miemie137 <635333704@qq.com>
2020-05-02 18:09:27 +0800

    漏斗图

--- src/module/echarts/view/bar.vue
+++ src/module/echarts/view/bar.vue
@@ -13,6 +13,8 @@ let echarts = require('echarts/lib/echarts');
 require('echarts/lib/chart/bar');
 // 引入折线图
 require('echarts/lib/chart/line');
+// 引入漏斗图
+require('echarts/lib/chart/funnel');
 // 引入提示框和标题组件
 require('echarts/lib/component/tooltip');
 require('echarts/lib/component/title');
@@ -54,7 +56,74 @@ export default {
             type: 'line',
             areaStyle: {}
         }]
-      }
+      },
+      {
+          width:300,
+          title: {
+            text: '漏斗图',
+            subtext: '纯属虚构'
+        },
+        tooltip: {
+            trigger: 'item',
+            formatter: "{a} <br/>{b} : {c}%"
+        },
+        toolbox: {
+            feature: {
+                dataView: {readOnly: false},
+                restore: {},
+                saveAsImage: {}
+            }
+        },
+        legend: {
+            data: ['展现','点击','访问','咨询','订单']
+        },
+
+        series: [
+            {
+                name:'漏斗图',
+                type:'funnel',
+                left: '10%',
+                top: 60,
+                //x2: 80,
+                bottom: 60,
+                width: '30%',
+                // height: {totalHeight} - y - y2,
+                min: 0,
+                max: 100,
+                minSize: '0%',
+                maxSize: '100%',
+                sort: 'descending',
+                gap: 2,
+                label: {
+                    show: true,
+                    position: 'inside'
+                },
+                labelLine: {
+                    length: 10,
+                    lineStyle: {
+                        width: 1,
+                        type: 'solid'
+                    }
+                },
+                itemStyle: {
+                    borderColor: '#fff',
+                    borderWidth: 1
+                },
+                emphasis: {
+                    label: {
+                        fontSize: 20
+                    }
+                },
+                data: [
+                    {value: 60, name: '访问'},
+                    {value: 40, name: '咨询'},
+                    {value: 20, name: '订单'},
+                    {value: 80, name: '点击'},
+                    {value: 100, name: '展现'}
+                ]
+            }
+        ]
+      },
       ]
     }
   },
